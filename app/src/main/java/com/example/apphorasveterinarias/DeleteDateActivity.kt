36990bb49ef6e14d29f2618f4b1127347e9ae9b6
Andres Perot <AndresPerotQ>
2022-05-30 15:01:08 -0400

    Ingreso de Activitys delete, update, y show

---
+++ app/src/main/java/com/example/apphorasveterinarias/DeleteDateActivity.kt
@@ -0,0 +1,11 @@
+package com.example.apphorasveterinarias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeleteDateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete_date)
+    }
+}
