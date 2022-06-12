0aa9424d103580378c7e1daf4c4818f062404c97
Andres Perot <andres.perot.quevedo@ciisa.cl>
2022-06-11 20:35:17 -0400

    Spinner

--- app/src/main/java/com/example/apphorasveterinarias/RegisterDateActivity.kt
+++ app/src/main/java/com/example/apphorasveterinarias/RegisterDateActivity.kt
@@ -2,9 +2,13 @@ package com.example.apphorasveterinarias
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.apphorasveterinarias.utils.TilValidator
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterDateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +16,48 @@ class RegisterDateActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register_date)
 
         val btnRegisterDate= findViewById<Button>(R.id.activity_register_date_button_check_in2)
+        val spnHours = findViewById<Spinner>(R.id.activity_register_date_spn_hora)
+        val spnRaces =findViewById<Spinner>(R.id.activity_register_date_spn_raza)
+        val tilpetname= findViewById<TextInputLayout>(R.id.activity_register_date_til_pet_name)
+        val tilcontact= findViewById<TextInputLayout>(R.id.activity_register_date_til_contact)
+
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.Hours_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spnHours.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.Races_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spnRaces.adapter = adapter
+        }
 
         btnRegisterDate.setOnClickListener{
-            Toast.makeText(this, "Su hora ha sido registrada", Toast.LENGTH_SHORT).show()
+
+            val namepet = tilpetname.editText?.text
+            val race= spnRaces.selectedItem.toString()
+            val hour= spnHours.selectedItem.toString()
+            val contact= tilcontact.editText?.text
+
+            val namepetValid = TilValidator(tilpetname).required().isValid()
+            val contacValid= TilValidator(tilcontact).required().contacto().isValid()
+
+            val intent= Intent(this,MainActivity::class.java)
+            if( namepetValid && contacValid ) {
+                Toast.makeText(this, "Su hora ha sido registrada", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Campos invalidos", Toast.LENGTH_SHORT).show()
+            }
+
 
         }
 
